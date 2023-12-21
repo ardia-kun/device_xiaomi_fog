86dbfd2d7b3a358a6cacf1d6d1d14614350294b4
Shawn Lin <shawnlin@google.com>
2023-12-21 12:58:21 +0000

    fog: Enable hide cutout emulations
    Bug: 317143921
    Test: make
    Flag: NONE
    Change-Id: I9f263bcdd33bfd61ba0241c1e7c4a90ccf4a8b1d
    
    [alternoegraha]:
    - I already did building NoCutoutOverlay, but I don't enable/have ro.support_hide_display_cutout prop and AvoidAppsInCutoutOverlay rro overlay.
    - Also move ro.support_hide_display_cutout to product.prop
    
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -320,7 +320,6 @@ PRODUCT_PACKAGES += \
     FrameworksFog \
     FrameworksFogIN2 \
     LineageSDKFog \
-    NoCutoutOverlay \
     NotchBarKiller \
     SettingsFog \
     SettingsProviderFog \
@@ -332,6 +331,10 @@ PRODUCT_PACKAGES += \
     SystemUIFog \
     TelephonyFog \
     WifiFog
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    AvoidAppsInCutoutOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
