a1596d1530776878b5ca99b687115fda8a4e9fe7
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2025-02-01 10:36:49 +0000

    fog: RvOS-15 Times
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- rvos_fog.mk
+++ rvos_fog.mk
@@ -9,13 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rvos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
+#RV-FLAGS
+RVOS_MAINTAINER := Kidz_𝙀𝙒™
+RVOS_BUILD_TYPE := Unofficial
 
 PRODUCT_NAME := lineage_fog
 PRODUCT_DEVICE := fog
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone -b refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b makeine-mk2 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b swordx/staging https://github.com/ardia-kun/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
 git clone -b lineage-22.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
@@ -15,3 +15,7 @@ cd kernel/xiaomi/fog
 rm -rf Android.bp
 rm -rf techpack/audio/Android.bp
 cd ../../..
+
+#Sign-Key
+rm -rf vendor/lineage-priv
+git clone https://github.com/ardia-kun/vendor -b 14-backup vlp && cp -R vlp/* vendor/ && rm -rf vlp
