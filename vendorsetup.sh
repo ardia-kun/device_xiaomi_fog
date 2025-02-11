6d53ecb3bc5eef45308356146a301bcdf332ccab
ardia-kun <ardiayogabekti@gmail.com>
2025-02-11 14:39:01 +0700

    fog: @SkylineUI-Reborn bringups
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fifteen https://github.com/ardia-kun/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone --depth=1 -b makeine-mk2 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b swordx/staging https://github.com/ardia-kun/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
 git clone -b lineage-22.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
@@ -15,3 +15,7 @@ cd kernel/xiaomi/fog
 rm -rf Android.bp
 rm -rf techpack/audio/Android.bp
 cd ../../..
+
+# Sign-Key
+rm -rf vendor/lineage-priv
+git clone https://github.com/ardia-kun/vendor -b 14-backup vlp && cp -R vlp/* vendor/ && rm -rf vlp
