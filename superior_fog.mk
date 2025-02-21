e2817ac25a7b57b4a28c7ffd041674e8d11af460
ardia-kun <ardiayogabekti@gmail.com>
2025-02-21 17:28:54 +0700

    fog: @superior-lts bringups
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- superior_fog.mk
+++ superior_fog.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
@@ -17,7 +17,10 @@ TARGET_SUPPORTS_QUICK_TAP := false
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := lineage_fog
+# Superior-OS
+DEVICE_MAINTAINER := Kidz_𝙀𝙒™
+
+PRODUCT_NAME := superior_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
