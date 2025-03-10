a5f8d4c274f85bbeaa7fec9292934cba67f0abf5
ardia-kun <ardiayogabekti@gmail.com>
2025-03-11 04:49:37 +0700

    RemovePackages: Remove some stuff pt 2
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,19 +5,24 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AudioFX \
     Aperture \
     BuiltInPrintService\
     Etar \
     ExactCalculator \
     EmergencyInfo \
     FM2 \
+    Jelly \
+    LiveWallpapersPicker \
     MusicPlayerGO \
     PrintRecommendationService \
     PrintSpooler \
     Recorder \
     SecureElement \
+    Seedvault \
     Stk \
     Traceur \
+    Twelve \
     MatLog \
     QuickAccessWallet \
     ViaBrowser\
