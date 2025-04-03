3b85d781ddaa75b7b230c9aa55640c090c880428
ardia-kun <ardiayogabekti@gmail.com>
2025-04-03 23:14:39 +0700

    vendorsetup.sh: Change kernel Tree
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,8 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Clone vendor tree
 git clone -b fifteen-qpr2 https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
-# Clone prebuilt kernel
-git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
+# Clone kernel
+git clone -b swordx/non-ksu --depth 1 https://github.com/ardia-kun/kernel_xiaomi_fog kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
 git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
